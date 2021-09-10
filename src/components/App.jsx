3f1df5a8dd8c0dd1ae23686aad0da7b3cea4dd42
Alexander <alrxander.goyra@gmail.com>
2021-09-10 04:24:35 +0300

    added fiels to start

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import { Searchbar } from './Searchbar';
+import { ImageGallery } from './ImageGallery';
+import { ImageGalleryItem } from './ImageGalleryItem';
+import { Loader } from './Loader';
+import { Button } from './Button';
+import { Modal } from './Modal';
+
+export class App extends Component {
+  state = {};
+
+  render() {
+    // const { contacts, filter } = this.state;
+    // const filteredContacts = this.getFilteredContacts();
+
+    return (
+      <div>
+        <h2>AAAAAAAAA</h2>
+      </div>
+    );
+  }
+}
